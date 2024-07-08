58c4a2ece15abbf354507d92acf2f106f4360f68
eviux.dev <eviux.dev@gmail.com>
2024-07-08 11:33:29 -0600

    feat: add api gateway - lambda integration

--- src/infra/stacks/lambda-stack.ts
+++ src/infra/stacks/lambda-stack.ts
@@ -1,15 +1,20 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { join } from 'path';
 
 export class LambdaStack extends Stack {
+  public readonly helloLambdaIntegration: LambdaIntegration;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    new Function(this, 'HelloWorldFunction', {
+    const helloLambda = new Function(this, 'HelloWorldFunction', {
       runtime: Runtime.NODEJS_18_X,
       handler: 'hello.handler',
       code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
     });
+
+    this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
   }
 }
